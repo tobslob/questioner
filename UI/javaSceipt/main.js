aee969bff215fdd002d735cf5f07e33a58d15df5
tobslob <kazmobileapp@gmail.com>
2019-01-09 12:22:12 +0100

    ch-add node.js eslint and airbnb

--- UI/javaSceipt/main.js
+++ UI/javaSceipt/main.js
@@ -1,143 +1,130 @@
-/*Toggle functionality for user profile*/
+/* Toggle functionality for user profile */
 function userToggle() {
-    'use strict';
-    let userlists = document.querySelector('.userlist');
-    const panelusers = document.querySelectorAll('.paneluser');
-    if(userlists) {
-        userlists.addEventListener('click', function (e) {
-            if (e.target.tagName == 'LI') {
+  const userlists = document.querySelector('.userlist');
+  const panelusers = document.querySelectorAll('.paneluser');
+  if (userlists) {
+    userlists.addEventListener('click', (e) => {
+      if (e.target.tagName == 'LI') {
+        const targetUserPanel = document.querySelector(e.target.dataset.target);
 
-                const targetUserPanel = document.querySelector(e.target.dataset.target);
-
-                panelusers.forEach(function (paneluser) {
-
-                    if (paneluser == targetUserPanel) {
-                        paneluser.classList.add('activerUserPanel');
-                    } else {
-                        paneluser.classList.remove('activerUserPanel');
-                    }
-                });
-            }
+        panelusers.forEach((paneluser) => {
+          if (paneluser == targetUserPanel) {
+            paneluser.classList.add('activerUserPanel');
+          } else {
+            paneluser.classList.remove('activerUserPanel');
+          }
         });
-    }
+      }
+    });
+  }
 }
 
 
-
-
-/*Toggle functionality for admin panel*/
+/* Toggle functionality for admin panel */
 function adminToggle() {
-    const lists = document.querySelector('.list');
-    const panels = document.querySelectorAll('.panel');
-    if(lists){
-        lists.addEventListener('click', function (e) {
-
-            if (e.target.tagName == 'LI') {
-
-                const targetPanel = document.querySelector(e.target.dataset.target);
+  const lists = document.querySelector('.list');
+  const panels = document.querySelectorAll('.panel');
+  if (lists) {
+    lists.addEventListener('click', (e) => {
+      if (e.target.tagName == 'LI') {
+        const targetPanel = document.querySelector(e.target.dataset.target);
 
 
-                panels.forEach(function (panel) {
-                    if (panel == targetPanel) {
-                        panel.classList.add('active');
-                    } else {
-                        panel.classList.remove('active');
-                    }
-                });
-
-            }
-
+        panels.forEach((panel) => {
+          if (panel == targetPanel) {
+            panel.classList.add('active');
+          } else {
+            panel.classList.remove('active');
+          }
         });
-    }
+      }
+    });
+  }
 }
 
 
 /* User registration */
-function registration(){
-    const regform = document.forms['regform'];
-    if(regform) {
-        regform.addEventListener('submit', function(e){
+function registration() {
+  const regform = document.forms.regform;
+  if (regform) {
+    regform.addEventListener('submit', (e) => {
+      e.preventDefault();
 
-            e.preventDefault();
+      const fullname = regform.querySelector('input[type="text"]')[1].value;
+      const username = regform.querySelector('input[type="text"]')[0].value;
+      const email = regform.querySelector('input[type="email"]').value;
 
-            const fullname = regform.querySelector('input[type="text"]')[1].value;
-            const username = regform.querySelector('input[type="text"]')[0].value;
-            const email = regform.querySelector('input[type="email"]').value;
+      // create element
+      const hFullname = document.createElement('h3');
+      const pUsername = document.createElement('P');
+      const pEmail = document.createElement('P');
 
-            //create element
-            const hFullname = document.createElement('h3');
-            const pUsername = document.createElement('P');
-            const pEmail = document.createElement('P');
-
-            // add document
-            hFullname.textContent = fullname;
-            pUsername.textContent = username;
-            pEmail.textContent = email;
-            // append to div
-            const myname = document.querySelector('.myprofil');
-            myname.appendChild(hFullname);
-            myname.appendChild(pUsername);
-            myname.appendChild(pEmail);
-        });
-    }
+      // add document
+      hFullname.textContent = fullname;
+      pUsername.textContent = username;
+      pEmail.textContent = email;
+      // append to div
+      const myname = document.querySelector('.myprofil');
+      myname.appendChild(hFullname);
+      myname.appendChild(pUsername);
+      myname.appendChild(pEmail);
+    });
+  }
 }
 
 /* Create a meetup record function */
 
 function createMeetUp() {
-    const createMeet = document.forms['reused_form Meetupform'];
-    if(createMeet){
-        createMeet.addEventListener('submit', function(e){
+  const createMeet = document.forms['reused_form Meetupform'];
+  if (createMeet) {
+    createMeet.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const title = createMeet.querySelector('#post-title meetup-title').value;
+      const time = createMeet.querySelector('#meetup-time').value;
+      const location = createMeet.querySelector('#post-location').value;
+      const post = createMeet.querySelector('#desc meetupdesc').value;
+      const tags = createMeet.querySelector('#post-tags').value;
+      const image = createMeet.querySelector('post-input').value;
+      // create element
+      const hTitle = document.createElement('h2');
+      const hLocation = document.createElement('h3');
+      const dateTime = document.createElement('h3');
+      const meetupPost = document.createElement('p');
+      const tag = document.createElement('p');
 
-            e.preventDefault();
-            const title = createMeet.querySelector('#post-title meetup-title').value;
-            const time = createMeet.querySelector('#meetup-time').value;
-            const location = createMeet.querySelector('#post-location').value;
-            const post = createMeet.querySelector('#desc meetupdesc').value;
-            const tags = createMeet.querySelector('#post-tags').value;
-            const image = createMeet.querySelector('post-input').value;
-            //create element
-            const hTitle = document.createElement('h2');
-            const hLocation = document.createElement('h3');
-            const dateTime = document.createElement('h3');
-            const meetupPost = document.createElement('p');
-            const tag = document.createElement('p');
-
-            //add document
-            hTitle.textContent = title;
-            hLocation.textContent = location;
-            dateTime.textContent = time;
-            meetupPost.textContent = post;
-            tag.textContent = tags;
-        });
-    }
+      // add document
+      hTitle.textContent = title;
+      hLocation.textContent = location;
+      dateTime.textContent = time;
+      meetupPost.textContent = post;
+      tag.textContent = tags;
+    });
+  }
 }
 
 
 // User Question/comment function
-function question(){
-    const questform = document.forms['questionform'];
+function question() {
+  const questform = document.forms.questionform;
 
-    if(questform){
-        questform.addEventListener('submit', function(e){
+  if (questform) {
+    questform.addEventListener('submit', (e) => {
+      e.preventDefault();
 
-            e.preventDefault();
+      const question = questform.querySelector('input[type="textare"]').value;
 
-            const question = questform.querySelector('input[type="textare"]').value;
+      const quest = document.createElement('p');
 
-            const quest = document.createElement('p');
+      quest.textContent = question;
 
-            quest.textContent = question;
-
-            document.querySelector('.questiondiv').appendChild(quest);
-
-        });
-    }
+      document.querySelector('.questiondiv').appendChild(quest);
+    });
+  }
 }
 
 userToggle();
-//question();
-//validate();
-//createMeetUp();
-//registration();
+// question();
+// validate();
+// createMeetUp();
+// registration();
 adminToggle();
