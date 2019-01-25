05ec7f52c868f92486e37fcc631c40499e4f2ee9
tobslob <kazmobileapp@gmail.com>
2019-01-25 11:49:51 +0100

    add create-up router

--- modules/v1/meetup.js
+++ modules/v1/meetup.js
@@ -1,4 +1,41 @@
 import express from 'express';
+import bodyparser from 'body-parser';
+import Joi from 'joi';
+import ImageJoi from 'joi-image-extension';
 import meetupdb from '../db/db';
 
-const app = express();
+const meetup = express();
+
+const ImgJoi = Joi.extend(ImageJoi);
+
+meetup.use(bodyparser.urlencoded({ extended: false }));
+meetup.use(bodyparser.json());
+
+
+/*
+ *
+*/
+meetup.post('/v1/create-meetup', (req, res) => {
+  const schema = Joi.object().keys({
+    id: meetupdb.meetuppost.length + 1,
+    createdOn: Joi.date().required(),
+    location: Joi.string().trim().required(),
+    images: ImgJoi.image().allowTypes(['png', 'bmp']),
+    topic: Joi.string().trim().required(),
+    body: Joi.string().trim().required(),
+    happeningOn: Joi.date().required(),
+    Tags: Joi.array().items(Joi.string().trim()).required(),
+  });
+
+  const data = Joi.validate(
+    req.body, schema, (err, result) => {
+      if (err) {
+        return res.status(400).send(err);
+      }
+      return res.status(200).send(result);
+    },
+  );
+  meetupdb.meetuppost.push(data);
+});
+
+module.exports = meetup;
