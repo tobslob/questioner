3431dec5b9a34d0ebbd14c1d15278a97bbc77fbf
tobslob <kazmobileapp@gmail.com>
2019-02-08 10:01:54 +0100

    travis configuration

---
+++ dist/rsvp-test.js
@@ -0,0 +1,28 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../app';
+
+// eslint-disable-next-line prefer-destructuring
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+/**
+ * Endpoint unit tests for rsvp api
+ */
+describe('/POST RSVP', () => {
+    const Data = {
+        response: 'maybe' || 'yes' || 'no',
+    };
+
+    it('should return status 422 error', (done) => {
+        chai.request(app)
+            .post('/v1/create-question')
+            .send(Data)
+            .end((err, res) => {
+                expect(res.status).be.equal(422);
+                done();
+            });
+    });
+});
