e45cb6f316eb5699dc678bcc29cfbcd298a68610
tobslob <kazmobileapp@gmail.com>
2019-02-13 23:20:06 +0100

    update core.js

--- modules/core.js
+++ modules/core.js
@@ -1,42 +1,42 @@
 import express from 'express';
-// import path from 'path';
+import path from 'path';
 
 const core = express();
 
 
-// core.use(express.static(path.join(__dirname, '../UI')));
+core.use(express.static(path.join(__dirname, '../UI')));
 
-// core.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'index.html'));
-// });
+core.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'index.html'));
+});
 
-// core.get('/login', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'sign-in.html'));
-// });
+core.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'sign-in.html'));
+});
 
-// core.get('/signup', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'sign-up.html'));
-// });
+core.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'sign-up.html'));
+});
 
-// core.get('/meetup-post', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'meetup-post.html'));
-// });
+core.get('/meetup-post', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'meetup-post.html'));
+});
 
-// core.get('/meetuppostpage', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'meetuppostpage.html'));
-// });
+core.get('/meetuppostpage', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'meetuppostpage.html'));
+});
 
-// core.get('/admin', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'admin.html'));
-// });
+core.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'admin.html'));
+});
 
-// core.get('/userprofile', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'userprofile.html'));
-// });
+core.get('/userprofile', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'userprofile.html'));
+});
 
-// core.get('/password-reset', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../UI', 'password-reset.html'));
-// });
+core.get('/password-reset', (req, res) => {
+    res.sendFile(path.join(__dirname, '../UI', 'password-reset.html'));
+});
 
 
 module.exports = core;
