a4f6b47850dad12d1dd2b83992e91b5013446226
tobslob <kazmobileapp@gmail.com>
2019-04-07 20:02:09 +0100

    tdd update

--- modules/test/user.js
+++ modules/test/user.js
@@ -1,42 +1,28 @@
 process.env.NODE_ENV === 'test';
-
-import chai from 'chai';
-import chaiHttp from 'chai-http';
+import { expect } from 'chai';
+import request from 'supertest';
 import app from '../../app';
 import faker from 'faker';
-import moment from 'moment';
-
-const { expect } = chai;
-// using chai-http middleware
-chai.use(chaiHttp);
 
 describe('POST USER', () => {
     it('Should successfully create a user account if inputs are valid', (done) => {
-        chai.request(app)
+        request(app)
             .post('/api/v1/user/signup')
-            .type('json')
             .send({
-                userImage: faker.image.people(),
-                firstName: 'Kazeem',
-                lastName: 'Odutola',
-                otherName: 'Oluwatobi',
-                email: 'tester@gmail.com',
-                password: 'Kazeem27',
-                userName: 'Kaz',
-                phoneNumber: '080874568356',
+                firstName: faker.name.firstName(),
+                lastName: faker.name.lastName(),
+                otherName: faker.name.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password(),
+                userName: faker.internet.userName(),
+                phoneNumber: faker.phone.phoneNumber(),
                 isAdmin: 'yes',
-                createdOn: moment(new Date())
+                userImage: faker.image.people(),
             })
-            .then((res) => {
-                // eslint-disable-next-line no-console
-                console.log(res);
-                const { body } = res;
-                expect(body).to.be.an('object');
-                expect(body.status).to.be.equals(201);
-                expect(body.data).to.be.an('object');
-                expect(body.data.token).to.be.a('string');
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.status).to.eql(200);
                 done();
-            })
-            .catch((error) => done(error));
+            });
     });
 });
